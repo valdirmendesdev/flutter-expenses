7ecebd21f956f3dcda5ebe60ace06a5c5d6a86e5
Valdir Mendes <valdir.mfjesus@yahoo.com.br>
2020-05-01 00:27:46 -0300

    Adding some styles

--- lib/main.dart
+++ lib/main.dart
@@ -6,25 +6,22 @@ void main() => runApp(ExpensesApp());
 class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyHomePage(), debugShowCheckedModeBanner: false,);
+    return MaterialApp(
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-
   final _transactions = [
     Transaction(
-      id: 't1',
-      title: 'Novo tênis de Corrida', 
-      value: 310.76, 
-      date: DateTime.now()
-    ),
+        id: 't1',
+        title: 'Novo tênis de Corrida',
+        value: 310.76,
+        date: DateTime.now()),
     Transaction(
-      id: 't2',
-      title: 'Conta de Luz', 
-      value: 211.30, 
-      date: DateTime.now()
-    ),
+        id: 't2', title: 'Conta de Luz', value: 211.30, date: DateTime.now()),
   ];
 
   @override
@@ -43,8 +40,42 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            child: Text('Lista de transações'),
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15.0,
+                        vertical: 10,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        )
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        tr.value.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple
+                        ),
+                      ),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(tr.title),
+                        Text(tr.date.toString()),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
           )
         ],
       ),
