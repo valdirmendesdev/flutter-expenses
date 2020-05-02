e5b1e74731246e1eb6ecfff434be7982b971c0d5
Valdir Mendes <valdir.mfjesus@yahoo.com.br>
2020-05-02 00:42:18 -0300

    Adding Text theme

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -42,10 +42,7 @@ class TransactionList extends StatelessWidget {
                   children: <Widget>[
                     Text(
                       tr.title,
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
+                      style: Theme.of(context).textTheme.title,
                     ),
                     Text(
                       DateFormat('d MMM y').format((tr.date)),
--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,24 @@ class ExpensesApp extends StatelessWidget {
       home: MyHomePage(),
       theme: ThemeData(
         primarySwatch: Colors.purple,
-        accentColor: Colors.amber 
+        accentColor: Colors.amber,
+        fontFamily: 'Quicksand',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          title: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 16.0,
+            fontWeight: FontWeight.bold,
+          )
+        ),
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(
+            title: TextStyle(
+              fontFamily: 'OpenSans',
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            )
+          )
+        ),
       ),
       debugShowCheckedModeBanner: false,
     );
